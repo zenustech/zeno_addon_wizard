e6957201dd8e6a1ac1870490cb16dd5ba26985cc
archibate <1931127624@qq.com>
2021-08-07 11:45:32 +0800

    adapt new run.py

--- run.py
+++ run.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python3
 
-import os
-import sys
 import runpy
 
-sys.path.insert(0, 'external/zeno')
-os.environ['PYTHONPATH'] = 'external/zeno' + os.pathsep + os.environ.get('PYTHONPATH', '')
-try:
-    from zenqt.main import main
-    sys.exit(main())
-except ImportError as e:
-    raise ImportError('please run `git submodule update --init --recursive`') from e
+runpy.run_path('external/zeno/run.py')
